ba9822e68365449109b1fce485c9032f12fc0d4a
Quentin Santos <qsantos@qsantos.fr>
2023-03-08 20:05:20 +0100

    Complete threads3

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -1,8 +1,6 @@
 // threads3.rs
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
@@ -24,11 +22,12 @@ impl Queue {
     }
 }
 
-fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
+fn send_tx(q: Queue, orig_tx: mpsc::Sender<u32>) -> () {
     let qc = Arc::new(q);
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
 
+    let tx = orig_tx.clone();
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
@@ -37,6 +36,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
         }
     });
 
+    let tx = orig_tx;
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
